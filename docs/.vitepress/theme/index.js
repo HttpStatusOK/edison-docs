128d1d6fca13807add4b7fd2edc0b57bb19f2ca9
HttpStatusOK <116876598+HttpStatusOK@users.noreply.github.com>
2024-09-06 18:39:52 +0800

    comment added

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,7 +3,10 @@ import './custom.css'
 
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/theme-chalk/dark/css-vars.css';
+
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 
 export default {
   ...DefaultTheme,
@@ -14,4 +17,28 @@ export default {
     // siteData is a ref of current site-level metadata.
     app.use(ElementPlus);
   },
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+    const route = useRoute();
+        
+    // giscus配置
+    giscusTalk({
+      repo: 'HttpStatusOK/edison-docs', //仓库
+      repoId: 'R_kgDOMc40zQ', //仓库ID
+      category: 'Announcements', // 讨论分类
+      categoryId: 'DIC_kwDOMc40zc4CiQGP', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    );
+  } 
 };
